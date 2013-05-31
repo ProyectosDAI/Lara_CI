34c535b0c10ef55d9b040240d0c7bb3c185c8612
Carlos Gude <carlos.sgude@gmail.com>
2013-05-31 16:20:54 +0200

    ginsneg -> app

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = 'ginseng';
+	$application_folder = 'app';
 
 /*
  * --------------------------------------------------------------------
